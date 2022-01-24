0666989f274475e278ef0523b7429c95247b6ddd
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-01-24 10:33:16 +0200

    Move to javascript

--- Practice/variables/variables_01.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-//define a variable "count" with value 0
-// Define a variable called count with an original value of 0 and then increment it (add 1 to it) on the following line.
--- Practice/variables/variables_02.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-//define a variable "ageLimit" with value 18 that cannot be re-assigned
-//Define a variable ageLimit that cannot be re-assigned and give it a value of 18.
